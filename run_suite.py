8df05aa4b361f448e7ab0934d7fd330928e70ea5
linnahan <1596695476@qq.com>
2023-02-08 09:40:52 +0800

    首次创建

--- run_suite.py
+++ run_suite.py
@@ -4,17 +4,14 @@ from htmltestreport import HTMLTestReport
 from tpshopweb_appTest.config import DIR_PATH
 
 # 判断是否有report、img、log这三个文件夹，没有的话创建
-report_path = DIR_PATH + os.sep + "report"
-img_path = DIR_PATH + os.sep + "img"
-log_path = DIR_PATH + os.sep + "log"
-if not os.path.exists(report_path):
-    os.makedirs(report_path)
-if not os.path.exists(img_path):
-    os.makedirs(img_path)
-if not os.path.exists(log_path):
-    os.makedirs(log_path)
+report_path = os.path.join(DIR_PATH, "report")
+img_path = os.path.join(DIR_PATH, "img")
+log_path = os.path.join(DIR_PATH, "log")
+os.makedirs(report_path, exist_ok=True)
+os.makedirs(img_path, exist_ok=True)
+os.makedirs(log_path,exist_ok=True)
 
 # 运行script文件夹脚本案例，并保存测试报告
 suite = unittest.defaultTestLoader.discover("./script")
-file_path = DIR_PATH + os.sep + "report" + os.sep + "tpshop_auto.html"
+file_path = os.path.join(DIR_PATH, "report", "tpshop_auto.html")
 HTMLTestReport(file_path).run(suite)
